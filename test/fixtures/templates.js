6937b96a5297fa9e3eae4c7089428700a3a2f8b2
ludohenin <ludovic.henin@yahoo.com>
2015-09-15 00:16:03 +0200

    Update Test 9

--- test/fixtures/templates.js
+++ test/fixtures/templates.js
@@ -85,9 +85,22 @@ export class App {}
 export class App {}
 
 // TEST_9
+import {Component, View, bootstrap} from 'angular2/angular2';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_BINDINGS} from 'angular2/router';
+// import {HTTP_BINDINGS} from 'http/http';
+
+import {Home} from './components/home/home';
+import {About} from './components/about/about';
+import {NamesList} from './services/NameList';
+
 @Component({
-  selector: 'app'
+  selector: 'app',
+  viewBindings: [Service]
 })
+@RouteConfig([
+  { path: '/', component: Home, as: 'home' },
+  { path: '/about', component: About, as: 'about' }
+])
 @View({
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
