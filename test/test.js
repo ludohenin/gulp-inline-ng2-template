09c49bd30ae1e908614895f13371191ba9020123
ludohenin <ludovic.henin@yahoo.com>
2015-10-17 18:31:13 +0200

    Cleanup

--- test/test.js
+++ test/test.js
@@ -1,18 +1,14 @@
 "use strict";
 
 var assert = require('assert');
-var extend = require('extend');
 var fs = require('fs');
-var join = require('path').join;
-var inline = require('../index');
 var File = require('vinyl');
+var inline = require('../index');
 
-var TEST_FILE = './test/fixtures/templates.js'
-var RESULT_EXPECTED = './test/fixtures/result_expected.js'
-var RESULT_ACTUAL = './test/fixtures/result_actual.js';
+var TEST_FILE       = './test/fixtures/templates.js';
+var RESULT_EXPECTED = './test/fixtures/result_expected.js';
+var RESULT_ACTUAL   = './test/fixtures/result_actual.js';
 
-// var result = parser(fs.readFileSync(TEST_FILE).toString(), { base: 'test/fixtures' });
-// fs.writeFileSync(RESULT_ACTUAL, result, 'utf8');
 
 describe('gulp-inline-ng2-template', function () {
   it('should simply work', function (done) {
@@ -24,11 +20,12 @@ describe('gulp-inline-ng2-template', function () {
     stream.write(jsFile);
 
     stream.once('data', function(file) {
+      var result = file.contents.toString();
       // Save the result in a file.
-      fs.writeFileSync(RESULT_ACTUAL, file.contents.toString(), 'utf8');
+      fs.writeFileSync(RESULT_ACTUAL, result, 'utf8');
 
       assert.equal(
-        file.contents.toString(),
+        result,
         fs.readFileSync(RESULT_EXPECTED).toString()
       );
       done();
