33e5bd2118c6f72f5c9e3e9e8534433e34daa9ee
mvisintin <mvisintin@travelrepublic.co.uk>
2015-12-07 13:08:08 +0000

    Jade | Added tests

---
+++ test/fixtures/result_actual-jade.js
@@ -0,0 +1,147 @@
+// TEST_1
+@Component({
+  selector: 'app'
+})
+@View({
+  template: `
+    <h1>Test</h1><p>Test</p>
+  `,
+  styles: [`
+    .test {
+      color: red;
+    }
+  `],
+  directives: [CORE_DIRECTIVES]
+})
+export class App {}
+
+// TEST_2
+@Component({
+  selector: 'app'
+})
+@View({
+  template: `
+    <h1>Test</h1><p>Test</p>
+  `,
+  styles: [`
+    .test {
+      color: red;
+    }
+  `]
+})
+export class App {}
+
+// TEST_3
+@Component({
+  selector: 'app'
+})
+@View({
+  template: `
+    <h1>Test</h1><p>Test</p>
+  `,
+  styles: [`
+    .test {
+      color: red;
+    }
+    .common {
+      color: grey;
+    }
+  `],
+  directives: [CORE_DIRECTIVES]
+})
+export class App {}
+
+// TEST_4
+@Component({
+  selector: 'app'
+})
+@View({
+  template: `
+    <h1>Test</h1><p>Test</p>
+  `,
+  styles: [`
+    .test {
+      color: red;
+    }
+    .common {
+      color: grey;
+    }
+    .test {
+      color: red;
+    }
+    .common {
+      color: grey;
+    }
+  `],
+  directives: [CORE_DIRECTIVES]
+})
+export class App {}
+
+// TEST_5
+@Component({
+  selector: 'app'
+})
+@View({
+  template: `
+    <h1>Test</h1><p>Test</p>
+  `,
+  styles: [`
+    .test {
+      color: red;
+    }
+    .common {
+      color: grey;
+    }
+  `],
+  directives: [CORE_DIRECTIVES]
+})
+export class App {}
+
+// TEST_6
+@Component({
+  selector: 'app'
+})
+@View({ template: `
+  <h1>Test</h1><p>Test</p>
+`, styles: [`
+  .test {
+    color: red;
+  }
+  .common {
+    color: grey;
+  }
+`] })
+export class App {}
+
+// TEST_7
+@Component({
+  selector: 'app'
+})
+@View({ template: `
+  <h1>Test</h1><p>Test</p>
+`, styles: [`
+  .test {
+    color: red;
+  }
+  .common {
+    color: grey;
+  }
+`], directives: [CORE_DIRECTIVES] })
+export class App {}
+
+// TEST_8
+@Component({
+  selector: 'app'
+})
+@View({ template: `
+  <h1>Test</h1><p>Test</p>
+`,
+  styles: [`
+    .test {
+      color: red;
+    }
+    .common {
+      color: grey;
+    }
+  `], directives: [CORE_DIRECTIVES] })
+export class App {}
--- test/test.js
+++ test/test.js
@@ -9,6 +9,10 @@ var TEST_FILE       = './test/fixtures/templates.js';
 var RESULT_EXPECTED = './test/fixtures/result_expected.js';
 var RESULT_ACTUAL   = './test/fixtures/result_actual.js';
 
+var JADE_TEST_FILE       = './test/fixtures/templates-jade.js';
+var JADE_RESULT_EXPECTED = './test/fixtures/result_expected-jade.js';
+var JADE_RESULT_ACTUAL   = './test/fixtures/result_actual-jade.js';
+
 
 describe('gulp-inline-ng2-template', function () {
   it('should simply work', function (done) {
@@ -27,6 +31,28 @@ describe('gulp-inline-ng2-template', function () {
       assert.equal(
         result,
         fs.readFileSync(RESULT_EXPECTED).toString()
+      );
+      done();
+    });
+
+  });
+
+  it('should simply work also with jade', function (done) {
+    var jsFile = new File({
+      contents: new Buffer(fs.readFileSync(JADE_TEST_FILE).toString())
+    });
+
+    var stream = inline({ base: 'test/fixtures', jade: true });
+    stream.write(jsFile);
+
+    stream.once('data', function(file) {
+      var result = file.contents.toString();
+      // Save the result in a file.
+      fs.writeFileSync(JADE_RESULT_ACTUAL, result, 'utf8');
+
+      assert.equal(
+        result,
+        fs.readFileSync(JADE_RESULT_EXPECTED).toString()
       );
       done();
     });
